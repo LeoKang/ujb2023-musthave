16c20a44afddf88e404970b5a90844924ef4ac20
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-11-28 11:23:10 +0900

    ~528page

--- MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
+++ MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -157,4 +157,40 @@ public class MVCBoardDAO extends DBConnPool {
 			psmt.executeUpdate();
 		}catch(Exception e) {}
 	}
+	
+	public boolean confirmPassword(String pass, String idx) {
+		boolean isCorr = true;
+		
+		try {
+			String sql = "SELECT COUNT(*) FROM mvcboard WHERE pass=? AND idx=?";
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, pass);
+			psmt.setString(2, idx);
+			rs = psmt.executeQuery();
+			rs.next();
+			if(rs.getInt(1)==0) {
+				isCorr = false;
+			}
+		}catch(Exception e) {
+			isCorr = false;
+			e.printStackTrace();
+		}
+		return isCorr;
+	}
+	
+	public int deletePost(String idx) {
+		int result = 0;
+		
+		try {
+			String query = "DELETE FROM mvcboard WHERE idx=?";
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, idx);
+			result = psmt.executeUpdate();
+		}catch(Exception e) {
+			System.out.println("게시물 삭제 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
 }
